1dc9477df6ed5afd64a8fe456d8507b6c58d27ef
Anthony Camarillo <58638902+a-camarillo@users.noreply.github.com>
2020-08-03 14:01:26 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# samthecookingguy-sentiment-analysis
+ Sentiment Analysis of comments on Sam the Cooking Guy's videos
---
+++ comments.py
@@ -0,0 +1,58 @@
+import os
+
+import googleapiclient.discovery
+import json
+
+api_service_name = "youtube"
+api_version = "v3"
+DEVELOPER_KEY = os.environ['GOOGLE_API']
+video_Id = '4tc4L85OyWQ'
+#create the youtube api object
+youtube = googleapiclient.discovery.build(
+    api_service_name, api_version, developerKey=DEVELOPER_KEY)
+
+def get_comment_threads(youtube,video_id,page_token=None):
+    '''This function gets comment thread data from the Comment Thread API endpoint''' 
+    results = youtube.commentThreads().list(
+        part = "snippet",
+        videoId = video_id,
+        textFormat = 'plainText',
+        maxResults = 100,
+        pageToken = page_token
+    ).execute()
+    return results
+
+def store_comments(comments):
+    '''extract data from Comment Thread response'''
+    for item in comments['items']:
+        comment = item['snippet']['topLevelComment']
+        author = comment_author.append(comment['snippet']['authorDisplayName'])
+        text = comment_text.append(comment['snippet']['textDisplay'])
+        time = comment_time.append(comment['snippet']['publishedAt'])
+    
+comment_author = []
+comment_text = []
+comment_time = []
+
+data_dict = {}
+
+
+try:
+    comments = get_comment_threads(youtube,video_id=video_Id)
+    next_page_token = comments['nextPageToken']
+    store_comments(comments)
+    while next_page_token:
+        comments = get_comment_threads(youtube,video_id=video_Id,page_token=next_page_token)
+        next_page_token = comments['nextPageToken']
+        store_comments(comments)
+except KeyError:
+    pass
+
+data_dict.update({'video_Id':[f'{video_Id}']})
+data_dict.update({'comment_author':comment_author})
+data_dict.update({'comment_text':comment_text})
+data_dict.update({'comment_time':comment_time})
+
+with open('meatball_sub','w') as file:
+    json.dump(data_dict, file)
+
